45a9d9cd094c9f1bbd145cca73939a052f78001d
Anarcom <you@example.com>
2021-09-15 14:38:41 +0000

    added docker img for dev server, building from server, not from docker hub

--- up.sh
+++ up.sh
@@ -1,2 +1,3 @@
-docker rmi $(sudo docker images --format '{{.Repository}}:{{.Tag}}' | grep 'anarcom')
+#docker rmi $(sudo docker images --format '{{.Repository}}:{{.Tag}}' | grep 'anarcom')
+docker-compose pull
 docker-compose up -d
